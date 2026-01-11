88cba3522fa3b0c7bb6fbb638ad31720a856623c
Marko Turchyn <marko.turchyn@gmail.com>
2026-01-10 23:52:55 -0500

    chore: update database script

--- database/init/01-init.sql
+++ database/init/01-init.sql
@@ -1,16 +1,12 @@
-CREATE USER auth_app_readwrite WITH PASSWORD 'auth_s3cr3t_k3y';
-CREATE USER shortener_app_readwrite WITH PASSWORD 'shortener_s3cr3t_k3y';
-CREATE USER audit_log_app_readwrite WITH PASSWORD 'audit_log_s3cr3t_k3y';
-
-CREATE DATABASE auth_service OWNER auth_app_readwrite;
-CREATE DATABASE shortener_service OWNER shortener_app_readwrite;
-CREATE DATABASE audit_log_service OWNER audit_log_app_readwrite;
+CREATE DATABASE auth_service;
+CREATE DATABASE shortener_service;
+CREATE DATABASE audit_log_service;
 
 \c auth_service
-CREATE SCHEMA auth AUTHORIZATION auth_app_readwrite;
+CREATE SCHEMA auth;
 
 \c shortener_service
-CREATE SCHEMA shortener AUTHORIZATION shortener_app_readwrite;
+CREATE SCHEMA shortener;
 
 \c audit_log_service
-CREATE SCHEMA audit_log AUTHORIZATION audit_log_app_readwrite;
+CREATE SCHEMA audit_log;
